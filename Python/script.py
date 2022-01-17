24ddd6f3b84bc04084bfdc7d68385315a61817af
Jandalf81 <janschmitte@gmail.com>
2022-01-17 21:13:47 +0100

    rebased project

--- Python/script.py
+++ Python/script.py
@@ -3,7 +3,7 @@ import configparser
 
 def __main__():
     config = configparser.ConfigParser()
-    config.read('/home/pi/CoinbaseScraper/CoinbaseScraper/Python/config.ini')
+    config.read('/home/pi/CoinbaseScraper/Python/config.ini')
 
     apiKey = config.get('Coinbase', 'apiKey')
     apiSecret = config.get('Coinbase', 'apiSecret')
