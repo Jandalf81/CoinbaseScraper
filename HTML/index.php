964ce7d1e2ee8f155370a44a8e172fec101eeda0
Jandalf81 <janschmitte@gmail.com>
2022-01-23 16:21:45 +0100

    index.php * added chart for current snapshot

--- HTML/index.php
+++ HTML/index.php
@@ -22,8 +22,49 @@
 
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
         <script type="text/javascript">
-            google.charts.load('current', {'packages':['corechart'], 'language':'de'});
-            google.charts.setOnLoadCallback(drawAccountDevelopment);
+            google.charts.load('current', {'packages':['table', 'corechart'], 'language':'de'});
+            google.charts.setOnLoadCallback(drawCharts);
+
+            function drawCharts() {
+                drawAccountSnapshot();
+                drawAccountDevelopment();
+            }
+
+            function drawAccountSnapshot() {
+                var data = new google.visualization.DataTable();
+                data.addColumn('string', 'Code');
+                data.addColumn('string', 'Name')
+                data.addColumn('number', 'Units');
+                data.addColumn('number', 'UnitPrice');
+                data.addColumn('number', 'Value');
+                data.addRows([
+<?php
+    $results = $db->query('SELECT * FROM snapshots ORDER BY timestamp DESC LIMIT 1');
+    while ($row = $results->fetchArray()) {
+        foreach($currencies as $currency) {
+            echo "\t\t\t\t\t['" . $currency . "', 'name', " . $row['units' . $currency] . ', ' . $row['unitPrice' . $currency] . ', ' . $row['sum' . $currency] . '],' . $rn;
+        }
+    }
+?>
+                ]);
+
+                var fCurrency = new google.visualization.NumberFormat({
+                    fractionDigits: 5,
+                    suffix: ' €'
+                });
+
+                var fFloat = new google.visualization.NumberFormat({
+                    fractionDigits: 5
+                });
+
+                // format the SUM
+                fFloat.format(data, 2);
+                fCurrency.format(data, 3);
+                fCurrency.format(data, 4);
+
+                var table = new google.visualization.Table(document.getElementById('divAccountSnapshot'));
+                table.draw(data, {showRowNumber: true, width: '100%', height: '100%'});
+            }
 
             function drawAccountDevelopment() {
                 var data = google.visualization.arrayToDataTable([
@@ -51,8 +92,10 @@
     # query database
     $results = $db->query($sql);
     while ($row = $results->fetchArray()) {
+        # add new entry
         echo "\t\t\t\t\t[new Date(" . $row['timestamp'] . '), ' . $row['sum'];
 
+        # add each currency
         foreach($currencies as $currency) {
             echo ', ' . $row[$currency];
         }
@@ -62,6 +105,7 @@
 ?>
                 ]);
 
+                // set options for chart
                 var options = {
                     title: 'Account Development',
                     curveType: 'none',
@@ -96,12 +140,30 @@
                     }
                 };
 
+                // prepare formatter
+                var fCurrency = new google.visualization.NumberFormat({
+                    //fractionDigits: 5,
+                    //suffix: ' €',
+                    pattern: '#,###.##### €'
+                });
+
+                // format the SUM
+                fCurrency.format(data, 0);
+<?php
+    # format each currency
+    foreach($currencies as $key => $currency) {
+        echo "\t\t\t\tfCurrency.format(data, " . ($key + 1) . ');' . $rn;
+    }
+?>
+
+                // draw chart
                 var chart = new google.visualization.LineChart(document.getElementById('divAccountDevelopment'));
                 chart.draw(data, options);
             }
         </script>
     </head>
     <body>
+        <div id="divAccountSnapshot" class="AccountSnapshot"></div>
         <div id="divAccountDevelopment" class="AccountDevelopment"></div>
     </body>
 </html>
